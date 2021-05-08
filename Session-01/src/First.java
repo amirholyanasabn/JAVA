53184a1744fda7ed5f83f55de92d323a4e44da2c
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-08 19:55:01 +0430

    session-01

---
+++ Session-01/src/First.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Java....");
+        String name = "Amirhossein";
+        System.out.println(name);
+        JFrame jFrame = new JFrame("Java Window");
+        jFrame.setVisible(true);
+        jFrame.setSize(500, 500);
+        float money = 12.99f;
+
+    }
+}
