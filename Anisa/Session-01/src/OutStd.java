258d5354e6139e568927a5f18b88366430a6a9b7
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-27 12:39:09 +0430

    Sessiom-01

---
+++ Anisa/Session-01/src/OutStd.java
@@ -0,0 +1,14 @@
+public class OutStd {
+    public static void main(String[] args) {
+        System.out.println("HELLO JAVA SE...");
+        System.out.print("Java is OOP .");
+        System.out.println();
+        System.out.printf("Hello %s , your account bank is %d and your balance is %2.3f","Amirhossein",111,289111012.256359874563255);
+    // single line comment
+        /*
+        multi
+        line
+        comment
+         */
+    }
+}
